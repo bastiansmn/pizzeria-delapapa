d6f0632d814799b0d9efea25c9292a086abc6a57
yaniakli <yaniakli9@gmail.com>
2022-05-01 04:45:29 +0200

    home page updated and is now full responsive

--- config/db.config.js
+++ config/db.config.js
@@ -2,6 +2,6 @@ module.exports = {
    user: process.env.DB_USER || 'postgres',
    host: process.env.DB_HOST || 'localhost',
    database: process.env.DB_NAME || 'pizzeria_delapapa',
-   password: process.env.DB_PASSWORD || 'postgres',
+   password: process.env.DB_PASSWORD || '2001',
    port: process.env.DB_PORT || 5432
 }

8abbb291836875f20944aa74f81474cd3cf7b915
bastiansmn <bastian.somon@gmail.com>
2022-05-24 12:14:30 +0200

    FIX

--- mappedControllers/order.controller.js
+++ mappedControllers/order.controller.js
@@ -6,6 +6,7 @@ module.exports = (app, db) => {
       db.query(query.createOrder(addressId))
          .then(result => {
             res.status(200).send(result.rows);
+            // TODO: send confirmation mail
          })
          .catch(err => {
             console.error(err);
@@ -16,9 +17,15 @@ module.exports = (app, db) => {
    });
    app.post("/order/addItem", (req, res) => {
       console.log(req.body, req.body.order_id);
-      db.query(query.insertItemToOrder(req.body.order_id, req.body.product_id, req.body.quant, req.body.pizza_size, req.body.sauce_name, req.body.drink_size))
+      db.query(query.insertItemToOrder(req.body.order_id, req.body.product_id, req.body.quant, req.body.type, req.body.pizza_size, req.body.sauce_name, req.body.drink_size))
          .then(result => {
             res.status(200).send(result.rows);
+         })
+         .catch(err => {
+            console.error(err);
+            res.status(500).send({
+               message: err
+            });
          })
    })
 }
--- queries/order.query.js
+++ queries/order.query.js
@@ -1,4 +1,4 @@
 module.exports = {
    createOrder: (addressId) => `INSERT INTO "order" (id, status, date, address_id) VALUES (DEFAULT, 'PENDING', NOW(), ${addressId}) RETURNING *`,
-   insertItemToOrder: (order_id, product_id, quant, pizza_size, sauce_name, drink_size) => `INSERT INTO prod_quant (order_id, product_id, quant, pizza_size, sauce_name, drink_size) VALUES (${order_id}, ${product_id}, ${quant}, '${pizza_size}', '${sauce_name}', '${drink_size}') RETURNING *`, 
+   insertItemToOrder: (order_id, product_id, quant, type, pizza_size, sauce_name, drink_size) => `INSERT INTO prod_quant (order_id, product_id, quant, pizza_size, sauce_name, drink_size) VALUES (${order_id}, ${product_id}, ${quant}, ${type}, '${pizza_size}', '${sauce_name}', '${drink_size}') RETURNING *`, 
 }
